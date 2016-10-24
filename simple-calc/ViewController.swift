9511f2db91813adfdf66ea97ce2d674952ab02ca
Yulong Tan <yulongtan67@gmail.com>
2016-10-24 16:40:19 -0700

    added number button touch up inside references

--- simple-calc/ViewController.swift
+++ simple-calc/ViewController.swift
@@ -20,6 +20,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var display: UITextView!
 
+    @IBAction func number1(_ sender: AnyObject) {
+        display.text = String(1)
+    }
+    @IBAction func number2(_ sender: AnyObject) {
+        display.text = String(2)
+    }
+    
+    @IBAction func number3(_ sender: AnyObject) {
+        display.text = String(3)
+    }
+
+    @IBAction func number4(_ sender: AnyObject) {
+        display.text = String(4)
+    }
+    
+    @IBAction func number5(_ sender: AnyObject) {
+        display.text = String(5)
+    }
+    
+    @IBAction func number6(_ sender: AnyObject) {
+        display.text = String(6)
+    }
+    
+    @IBAction func number7(_ sender: AnyObject) {
+        display.text = String(7)
+    }
+    
+    @IBAction func number8(_ sender: AnyObject) {
+        display.text = String(8)
+    }
+    
+    @IBAction func number9(_ sender: AnyObject) {
+        display.text = String(9)
+    }
+    @IBAction func clear(_ sender: AnyObject) {
+        display.text = ""
+    }
 }
 
